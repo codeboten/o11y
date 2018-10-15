b3feffbbe2491433013c4a655a601cd3f0f17d8a
Alex Ianchici <rawr@alrex.ca>
2018-10-14 19:55:11 -0700

    adding tracing to the mars station

--- station-mars/index.js
+++ station-mars/index.js
@@ -1,17 +1,41 @@
 'use strict';
 
-// startTrace starts the trace
+var beeline = require("honeycomb-beeline")({
+  writeKey: process.env.HONEYCOMB_KEY,
+  dataset: process.env.HONEYCOMB_DATASET
+});
+
+
+// startTrace returns a trace object
 function startTrace(req) {
+  let traceInfo = {
+    application: "station-api",
+    platform: "gcp",
+    name: "handleRequest"
+  };
+
+  if (req.headers["x-honeycomb-trace"]) {
+    let { traceId, parentSpanId } = beeline.unmarshalTraceContext(
+      req.headers["x-honeycomb-trace"]
+    );
+    return beeline.startTrace(
+      traceInfo,
+      traceId,
+      parentSpanId
+    );
+  }
+  return beeline.startTrace(traceInfo);
 }
 
 exports.http = (request, response) => {
-  startTrace(request);
+  let trace = startTrace(request);
 
   let output = {
     planet: "mars",
     weather: "it's kinda cold here"
   };
 
+  beeline.finishTrace(trace);
   response.status(200).send(JSON.stringify(output));
 };
 
